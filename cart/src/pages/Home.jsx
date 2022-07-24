27d9a039af796f9c3570cd81c550928639ce0943
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-07-23 23:33:07 -0400

    cart started
    app set

---
+++ cart/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import './Home.css';
+const Home = () => {
+    return (
+        <div className='home-page'>
+           <p className= 'text-danger'> Home Page </p>
+        </div>
+    )
+}
+
+export default  Home
