916bffa11c035b9d74308f04279e230ecde3569d
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-08-26 22:20:03 -0400

    Update Card
    card props fix

--- cart/src/components/Cards.jsx
+++ cart/src/components/Cards.jsx
@@ -1,14 +1,21 @@
 const Cards = (props) => {
     return (
         <>
-           <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-3 mb-4">
-                <card className="h-100 shadow">
+           <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mb-4">
+                <card className="card h-100 shadow">
                     <img src={props.img} alt="" className="card img-top img-fluid" />
-                    <card className="body">
-                        <h5 className="card-title">{props.title}</h5>
-                        <p className="card-text">{props.desc}</p>
-                        <button className="btn btn-success">Aggiungi al carrello</button>
-                    </card>
+                    <div className="card-body">
+                        <div className= "d-flex justify-content-between">
+                            <h5 className="card-title">{props.title}</h5>
+                            <span className="fw-folder">${props.price}</span>
+                        </div>
+                        <div className="">
+                            <p className="card-text">{props.desc}</p>
+                        </div>
+                        <div className="d-grid justify-content-end mt-4">
+                            <button className="btn btn-sm btn-outline-success">Aggiungi al carrello</button>
+                        </div>    
+                    </div>
                 </card>
            </div>
         </>
