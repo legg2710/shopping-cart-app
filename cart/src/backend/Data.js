0fb2e915e24507c1edbb9f071f48581f9561e207
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-08-28 19:36:42 -0400

    Update Cart
    Cart Settings

--- cart/src/backend/Data.js
+++ cart/src/backend/Data.js
@@ -10,28 +10,28 @@ const data = {
             img: imgOne,
             title: 'Profiterolo',
             desc: 'Una pasta frolla a forma di ciambella, aperta al centro e farcita con crema, panna o tartufo, solitamente servita con sopra cioccolata calda.',
-            price: 13,
+            price: 4,
         },
         {
             id: 2,
             img: imgTwo,
             title: 'Marchesa Di Cocco',
             desc: 'Delle sfoglie di biscotti imbevute di cioccolato bianco, cocco e caramello salato, guarnito con scaglie di cocco disidratato.',
-            price: 11,
+            price: 6,
         },
         {
             id: 3,
             img: imgThree,
             title: "Torta d'Opera",
             desc: 'Un pan di spagna a base di mandorle che viene inzuppato in uno sciroppo a base di caffè e intervallato da strati di crema al burro al caffè e ganache al cioccolato.',
-            price: 65,
+            price: 6,
         },
         {
             id: 4,
             img: imgFour,
             title: 'Marchesa Di Cioccolato',
             desc: 'Delle sfoglie di biscotti imbevute di salsa al latte ricoperte di cioccolato fondente accompagnata da fragole e noci al forno.',
-            price: 7,
+            price: 6,
         },
       
     ]
--- cart/src/components/Cart.jsx
+++ cart/src/components/Cart.jsx
@@ -18,10 +18,26 @@ const Cart = () => {
         <div className='container-fluid py-5'>
             <div className="row">
                 <h4 className="text-center py-3 text-decoration-underline">La Mia Carta</h4>
-                <div className="col-12">
-                    <div className="d-flex justify-content-center">
-                        <p className='position-relative fw-bolder text-title fs-5'>Carrello <span className="position-absolute translate-middle rounded-pill badge bg-danger mx-1">{totalUniqueItems}</span></p>
-                        <p className='fw-bolder text-title fs-5'>Totale Articoli <span className="position-absolute translate-middle rounded-pill badge bg-success mx-1">{totalItems}</span></p>
+                <div className="col-12 py-4 shadow">
+                    <div className="d-flex justify-content-center py-3">
+                        <p className='position-relative fw-bolder text-title'>Carrello <span className="position-absolute translate-middle rounded-pill badge bg-danger mx-1">{totalUniqueItems}</span></p>
+                        <p className='fw-bolder text-title'>Totale Articoli <span className="position-absolute translate-middle rounded-pill badge bg-success mx-1">{totalItems}</span></p>
+                    </div>
+                    <div>
+                      <table className="table table-primary table-hover m0">
+                        <tbody>
+                            {items.map((item, index) => {
+                              return(
+                                  <tr key={index}>
+                                      <td><img src={item.img} className='img-cart' alt={item.title} /></td>
+                                      <td> {item.title}</td>
+                                      <td>$ {item.price}</td>
+                                      <td>Quantità: {item.quantity}</td>
+                                  </tr>
+                              )
+                            })}
+                        </tbody>
+                      </table>
                     </div>
               </div>
           </div>
