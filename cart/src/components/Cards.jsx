00a8df2cefe96e337e1f806977b7e52fe0163db7
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-09-03 15:02:33 -0400

    Cart.css Update
    Adding some css

--- cart/src/components/Cards.jsx
+++ cart/src/components/Cards.jsx
@@ -5,19 +5,19 @@ const Cards = (props) => {
     const {addItem} = useCart();
     return (
         <>
-           <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mb-4">
-                <card className="card h-100 shadow rounded">
+           <div className="body col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mb-4">
+                <card className="body card h-100 shadow rounded">
                     <img src={props.img} alt="" className="card img-top img-fluid" />
                     <div className="card-body">
                         <div className= "d-flex justify-content-between">
-                            <h5 className="card-title">{props.title}</h5>
-                            <span className="fw-folder">${props.price}</span>
+                            <h5 className="text card-title">{props.title}</h5>
+                            <span className="text-second fw-folder">${props.price}</span>
                         </div>
-                        <div className="">
-                            <p className="card-text">{props.desc}</p>
+                        <div className="body-second">
+                            <p className="text-second card-text">{props.desc}</p>
                         </div>
                         <div className="d-grid justify-content-end mt-4">
-                            <button className="btn btn-sm btn-outline-success" onClick={() => addItem (props.item)} >Aggiungi al carrello</button>
+                            <button className="text-second btn btn-sm btn-outline-success" onClick={() => addItem (props.item)} >Aggiungi al carrello</button>
                         </div>    
                     </div>
                 </card>
--- cart/src/pages/Home.jsx
+++ cart/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import data from '../backend/Data';
 import './Home.css';
 const Home = () => {
     return (
-        <div className='container-fluid py-20px'>
-           <h3 className= 'text-center mt-5'> Dolci</h3>
+        <div className='body container-fluid py-20px'>
+           <h3 className= 'text text-center mt-5'> Dolci</h3>
            <div className="container py-4">
             <div className="row">
                 {data.products.map((item,index) => {
