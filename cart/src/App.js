ee635976eff868628a960eb0fff00b39ce2addb1
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-09-18 17:20:43 -0300

    Navbar insert
    Navbar set

--- cart/src/App.js
+++ cart/src/App.js
@@ -3,10 +3,12 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import Cart from './components/Cart';
 import { CartProvider } from 'react-use-cart';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <CartProvider>
         <Home />
         <Cart />
---
+++ cart/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className="nav body">
+        <a href="/" className="site-title">Il Bagliore</a>
+        <ul>
+            <li>
+                <a href="/menu">Menù</a>
+            </li>
+            <li>
+                <a href="/contacto">Contattaci</a>
+            </li>
+        </ul>
+    </nav>
+}
