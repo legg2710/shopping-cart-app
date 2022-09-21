42df955c9ea4adbce6dc8c095f865c61f721111e
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-09-20 22:44:46 -0300

    Navbar update
    React Routes

--- cart/src/App.js
+++ cart/src/App.js
@@ -6,25 +6,21 @@ import { CartProvider } from 'react-use-cart';
 import Navbar from './components/Navbar';
 import Menu from './pages/Menu';
 import Contacto from './pages/Contacto';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  let component
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />
-      break 
-    case "/menu":
-      component = <Menu />
-        break
-    case "/contacto":
-      component = <Contacto />
-  }
   return (
     <div>
       <>
       <Navbar />
-      <div> </div>
-     {component}
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home />}/>  
+          <Route path='/menu' element={<Menu />}/>  
+          <Route path='/contacto' element={<Contacto />}/>  
+        </Routes>  
+      </div>
+   
       </>
       
       <CartProvider>
--- cart/src/components/Navbar.jsx
+++ cart/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+ 
 export default function Navbar() {
-    return <nav className="nav body">
-        <a href="/" className="site-title">Il Bagliore</a>
-        <ul>
-            <li>
-                <a href="/menu">Menù</a>
-            </li>
-            <li>
-                <a href="/contacto">Contattaci</a>
-            </li>
-        </ul>
-    </nav>
+    return (
+        <nav className="nav body">
+            <Link to="/" className="site-title">
+                Il Bagliore
+            </Link>
+            <ul>
+                <CustomLink to="/menu">Menù</CustomLink>
+                <CustomLink to="/contacto">Contattaci</CustomLink>
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink ({ to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+        
+    )
 }
