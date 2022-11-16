325b7d918c2afd0111ac57c8c137f97fb3c46d19
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-11-16 00:14:06 -0300

    Signup.js Update
    css fixes on signup button and login div

--- cart/src/components/Signup.js
+++ cart/src/components/Signup.js
@@ -12,32 +12,33 @@ export default function Signup() {
         <Container className="d-flex align-times-center justify-content-center" style={{ minHeight: "100vh" }}>
             
             <div className="w-100" style={{ maxWidth: "400px" }}>
-            <Card>    
-                <Card.Body>
-                    <h2 className='text-center mb-4'>Sign Up</h2>
-                    <Form>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required/>
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required/>
-                        </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmationRef} required/>
-                        </Form.Group>
-                        <Button className="w-100 mt-4" type="submit">Sign Up</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
+                <Card className='body'>    
+                    <Card.Body>
+                        <h2 className='text-second text-center mb-4'>Sign Up</h2>
+                        <Form>
+                            <Form.Group id="email">
+                                <Form.Label className='text-second'>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required/>
+                            </Form.Group>
+                            <Form.Group id="password">
+                                <Form.Label className='text-second'>Password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef} required/>
+                            </Form.Group>
+                            <Form.Group id="password-confirm">
+                                <Form.Label className='text-second'>Password Confirmation</Form.Label>
+                                <Form.Control type="password" ref={passwordConfirmationRef} required/>
+                            </Form.Group>
+                            <Button className="body text-second btn btn-sm btn-outline-success w-100 mt-4" type="submit">Sign Up</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+                <div className='w100 text-second text-center mt-2'>
+                    Already have and account? Log In
+                </div>
             </div>
         </Container>
 
-            <div className='w100 text-center mt-2'>
-                Already have and account? Log In
-            </div>
+
     </>
   )
 }
