66f54dd9a82d94987b6f9d6a0ad39d96fff1aedd
Luis Gonzalez <83027075+legg2710@users.noreply.github.com>
2022-11-15 23:51:13 -0300

    Update Signup.js
    css form fixes

--- cart/src/components/Signup.js
+++ cart/src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import { Form, Button, Card } from "react-bootstrap"
+import { Form, Button, Card, Container } from "react-bootstrap"
 
 export default function Signup() {
 
@@ -9,29 +9,32 @@ export default function Signup() {
 
   return (
     <>
-        <Card className="d-flex align-times-center justify-content-center" style={{ minHeight: "50vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-            <Card.Body>
-                <h2 className='text-center mb-4'>Sign Up</h2>
-                <Form>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} required/>
-                    </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} required/>
-                    </Form.Group>
-                    <Form.Group id="password-confirm">
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" ref={passwordConfirmationRef} required/>
-                    </Form.Group>
-                    <Button className="w-100" type="submit">Sign Up</Button>
-                </Form>
-            </Card.Body>
-	    </div>
+        <Container className="d-flex align-times-center justify-content-center" style={{ minHeight: "100vh" }}>
+            
+            <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Card>    
+                <Card.Body>
+                    <h2 className='text-center mb-4'>Sign Up</h2>
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required/>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required/>
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmationRef} required/>
+                        </Form.Group>
+                        <Button className="w-100 mt-4" type="submit">Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            </div>
+        </Container>
 
-        </Card>
             <div className='w100 text-center mt-2'>
                 Already have and account? Log In
             </div>
